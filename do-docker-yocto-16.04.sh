5793c16417cd9873388fe66f68a2573e5c4e0cd2
Jim Binder <jbinder6342@gmail.com>
2020-07-13 13:42:24 -0400

    remove docker instance on exit

--- do-docker-yocto-16.04.sh
+++ do-docker-yocto-16.04.sh
@@ -1,13 +1,18 @@
 #!/bin/bash 
 
-YOCTO_HOME_DIR="/home/jbinder/Development/spirent-dev/yocto/yocto-spirent-git-3.0"
+YOCTO_HOME_DIR=~/Development/spirent-dev/yocto/yocto-spirent-git-3.0
 
 DOCKER_TAG=ubuntu-16.04
 #DOCKER_TAG=ubuntu-18.04
 #DOCKER_TAG=ubuntu-20.04
 
+if [ ! -e "$YOCTO_HOME_DIR" ]; then 
+    echo "Missing Yocto home dir: $YOCTO_HOME_DIR" 
+    exit 1 
+fi 
+
 #docker run --rm -it -v $YOCTO_HOME_DIRE:/yocto crops/poky --workdir=/yocto
-docker run -it -v $YOCTO_HOME_DIR:/yocto crops/poky:$DOCKER_TAG  --workdir=/yocto
+docker run -rm -it -v $YOCTO_HOME_DIR:/yocto crops/poky:$DOCKER_TAG  --workdir=/yocto
 
 
 
