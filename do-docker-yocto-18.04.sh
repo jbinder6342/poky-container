d3ecf1844603db15565d2f656366b8b98d33d524
Jim Binder <jbinder6342@gmail.com>
2020-07-14 17:17:50 -0400

    Added ubuntu-18.04, fixed --rm syntax error.

--- do-docker-yocto-18.04.sh
+++ do-docker-yocto-18.04.sh
@@ -2,8 +2,8 @@
 
 YOCTO_HOME_DIR=~/Development/spirent-dev/yocto/yocto-spirent-git-3.0
 
-DOCKER_TAG=ubuntu-16.04
-#DOCKER_TAG=ubuntu-18.04
+#DOCKER_TAG=ubuntu-16.04
+DOCKER_TAG=ubuntu-18.04
 #DOCKER_TAG=ubuntu-20.04
 
 if [ ! -e "$YOCTO_HOME_DIR" ]; then 
